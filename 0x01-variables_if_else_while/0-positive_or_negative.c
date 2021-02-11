0538ec888f339f0fa72de24f89cd27df7e309cba
Omegagen-Alpha <55996482+Omegagen-Alpha@users.noreply.github.com>
2021-02-10 22:47:56 -0600

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-/* Main Function  will assign a random number to
+/* function main will assign a random number to
 **the variable n each time it is executed.*/
 /* betty style doc for function main goes there */
 
