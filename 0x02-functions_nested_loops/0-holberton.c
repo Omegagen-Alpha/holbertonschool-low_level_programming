3de5e4ee40d88b1cd1c13fc757e060de33f90ee0
Kendrick Campbell <kendrickjr2002@gmail.com>
2021-06-28 21:54:08 -0700

    fiel 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,7 +1,35 @@
-#include <stdio.h>
-
-int main()
+#include <stdio.h> 
+#include "holberton.h"
+int main(void)
 {
-	printf("Holberton\n");
+	char h;
+	char o;
+	char l;
+	char b;
+	char e;
+	char r;
+	char t;
+	char n;
+
+	h = 'H';
+	o = 'o';
+	l = 'l';
+	b = 'b';
+	e = 'e';
+	r = 'r';
+	t = 't';
+	n = 'n';
+
+	_putchar(h);
+	_putchar(o);
+	_putchar(l);
+	_putchar(b);
+	_putchar(e);
+	_putchar(r);
+	_putchar(t);
+	_putchar(o);
+	_putchar(n);
+	_putchar('\n');
+
 	return (0);
 }
