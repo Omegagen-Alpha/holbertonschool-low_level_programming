15d7db8beb19a7b264e2b2856ac69b469a9fd005
Kendrick Campbell <kendrickjr2002@gmail.com>
2021-06-30 01:10:30 -0700

    file 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - function main balblabla
+*
+* Return: return balblabva
+*/
+
+int main(void)
+{
+	long int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+
+	return (0);
+}
