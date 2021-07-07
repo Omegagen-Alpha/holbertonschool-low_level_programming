4b75e953b45481d3e0d42f21d5e184f0ee310e49
Kendrick Campbell <kendrickjr2002@gmail.com>
2021-07-07 08:25:20 -0700

    file 9 added

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - function does fizzbuzz nonsense
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if (x % 3 && x % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", x);
+		}
+		if (x != 100)
+		{
+			putchar(' ');
+		}
+		else 
+		{
+			putchar('\n');
+		}
+	}
+	return (0);
+}
