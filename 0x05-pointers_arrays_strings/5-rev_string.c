edbce72803a9248f8561b102e11791f7b9f7351f
Kendrick Campbell <kendrickjr2002@gmail.com>
2021-07-06 21:53:45 -0700

    fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,21 +5,4 @@
  */
 
 void rev_string(char *s)
-{
-	{
-		int i;
-		int len = 0;
-		char c;
-		if (!s)
-		return NULL;
-		while(s[len] != '\0')
-		{
-		len++;
-		}
-		for(i = 0; i < (len/2); i++)
-		{
-			c = s[i];
-			s[i] = s[len - i - 1];
-			s[len - i - 1] = c;
-		}
-		return str;
+{	
