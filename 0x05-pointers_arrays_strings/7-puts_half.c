d18d046a24bae081588188dcfa4bfbfe2c56ba96
Kendrick Campbell <kendrickjr2002@gmail.com>
2021-07-07 17:24:33 -0700

    file 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * puts_half - function prints the second part of a string
+ *
+ * @str: pointer of the string
+ *
+ * Return:
+ */
+
+void puts_half(char *str)
+{
+	int l;
+	i = 0;
+	
+	while (str[i] != 0)
+	{
+		i++;
+	}
+	l = i;
+	for (i = (l - 1) / 2 + 1; i < l; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
