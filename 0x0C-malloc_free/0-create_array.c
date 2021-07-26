f796c91d4bf70296cf305d1d4cd2518631566eae
Kendrick Campbell <kendrickjr2002@gmail.com>
2021-07-25 22:10:03 -0700

    add

---
+++ 0x0C-malloc_free/0-create_array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holbeerton.h"
+
+/**
+ * *create_array - creates an array of characters and initializes it with a specific char
+ * @size: the adress of memory
+ * @C: 
+ *
+ * Return: Null if size = 0, and the array itself otherwise
+ */
+
+char *create_array(unsigned int size, char C)
+{
+	ar = malloc(size * sizeof(
+}
